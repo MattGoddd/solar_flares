672cb362af88c1a551c6169a363bfc361cf2746c
mattgoddd <mattgoh2004@gmail.com>
2025-04-29 16:46:37 +0800

    data sorted for now

--- src/models/flare_prediction/data_loader.py
+++ src/models/flare_prediction/data_loader.py
@@ -9,44 +9,97 @@ import pandas as pd
 import sunpy.map
 import sunpy.io
 import os
+from sunpy.net import attrs as a, hek
+from sunpy.time import parse_time
 
-# Establish connection to JSOC
-
-c = drms.Client()
-
-# Define the series
-print(c.series(r'hmi\.sharp_'))
-
-si = c.info('hmi.sharp_cea_720s_nrt')
-
-keywords = list(si.keywords.index)
-print(keywords)
-
-end_time = datetime.now(timezone.utc)
-
-start_time = end_time - timedelta(hours = 2)
-
-jsoc_time_format = "%Y.%m.%d_%H:%M:%S_TAI"
-start_str = start_time.strftime(jsoc_time_format)
-end_str = end_time.strftime(jsoc_time_format)
-
-keys = c.query(f'hmi.sharp_cea_720s_nrt[][{start_str} - {end_str}][]', key = keywords)
-
-print(keys)
-
-output_dir = "data/sharp_csv"
-os.makedirs(output_dir, exist_ok=True)
-
-df = pd.DataFrame(keys)
-df.to_csv(os.path.join(output_dir, "nrt_sharp"), index = False)
-
-def parse_tai_string(tstr, datetime=True):
+def parse_tai_string(tstr, return_datetime=True):
     year   = int(tstr[:4])
     month  = int(tstr[5:7])
     day    = int(tstr[8:10])
     hour   = int(tstr[11:13])
     minute = int(tstr[14:16])
-    if datetime: 
+    if return_datetime: 
         return datetime(year,month,day,hour,minute)
     else: 
         return year,month,day,hour,minute
+
+# Establish connection to JSOC
+
+def data_loader(number_of_days = 1):
+    c = drms.Client()
+
+    # Define the series
+    print(c.series(r'hmi\.sharp_'))
+
+    si = c.info('hmi.sharp_cea_720s')
+
+    end_time = datetime.now(timezone.utc) - timedelta(days = 465)
+
+    start_time = end_time - timedelta(days = 1)
+
+    # Initialize or load existing DataFrame and CSV
+    output_dir = "data/sharp_csv"
+    os.makedirs(output_dir, exist_ok=True)
+    csv_path = os.path.join(output_dir, "training_data.csv")
+
+    if os.path.exists(csv_path):
+        combined_df = pd.read_csv(csv_path)
+    else:
+        combined_df = pd.DataFrame()
+
+    for i in range(number_of_days):
+        start_time = start_time - timedelta(days=1)
+        end_time = end_time - timedelta(days=1)
+
+        jsoc_time_format = "%Y.%m.%d_%H:%M:%S_TAI"
+        start_str = start_time.strftime(jsoc_time_format)
+        end_str = end_time.strftime(jsoc_time_format)
+
+        # Loading the X_train data
+        data = c.query(f'hmi.sharp_cea_720s[][{start_str} - {end_str}@1h][]', 
+                    key='T_REC, NOAA_AR, USFLUX,TOTUSJH,TOTPOT,MEANGBT,MEANGBZ,MEANJZD,MEANPOT,MEANSHR,SHRGT45,R_VALUE,AREA_ACR'
+        )
+
+        df = pd.DataFrame(data)
+        key_cols = ['T_REC', 'NOAA_AR', 'USFLUX', 'TOTUSJH', 'TOTPOT', 'MEANGBT', 'MEANGBZ', 'MEANJZD', 'MEANPOT', 'MEANSHR', 'SHRGT45', 'R_VALUE', 'AREA_ACR']
+        df_filtered = df[(df[key_cols] != 0).all(axis=1)].copy()
+        df_filtered['T_REC'] = df_filtered['T_REC'].apply(parse_tai_string)
+
+        # Loading the Y_train data
+        client = hek.HEKClient()
+        flare_data = client.search(
+            a.Time(start_time, end_time),
+            a.hek.FL,
+        )
+
+        names = [name for name in flare_data.colnames if len(flare_data[name].shape) <= 1]
+        flare_data_flat = flare_data[names]
+        flare_data_df = flare_data_flat.to_pandas()
+
+        flare_data = flare_data_df[['ar_noaanum', 'fl_goescls', 'event_peaktime']]
+        flare_data = flare_data[
+            (flare_data['ar_noaanum'] > 0) & (flare_data['fl_goescls'] != '')]
+
+        df_filtered['flare_label'] = "None"
+
+        for idx, flare in flare_data.iterrows():
+            ar = flare['ar_noaanum']
+            flare_time = flare['event_peaktime']
+            flare_class = flare['fl_goescls']
+
+            match = (df_filtered['NOAA_AR'] == ar) & (df_filtered['T_REC'] <= flare_time) & (flare_time <= df_filtered['T_REC'] + pd.Timedelta(hours=24))
+
+            label = flare_class
+            df_filtered.loc[match, 'flare_label'] = label
+
+        # Append to combined DataFrame
+        combined_df = pd.concat([combined_df, df_filtered], ignore_index=True)
+
+        print(f"Day: {i + 1}/{number_of_days} processed")
+
+    # Save the combined DataFrame to CSV
+    combined_df.to_csv(csv_path, index=False)
+
+    print(combined_df)
+
+data_loader(300)
